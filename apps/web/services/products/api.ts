7d47791b203554f951dfc8cee320d964d195a7ce
Hamed <hamed.karimian@arvancloud.ir>
2025-04-09 19:20:12 +0330

    refactor(api): Move api to env file

--- apps/web/services/products/api.ts
+++ apps/web/services/products/api.ts
@@ -1,16 +1,16 @@
 import apiClient from '../api/axios';
 import { Product } from './types';
 
-const BASE_URL = 'https://fakestoreapi.com/products';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || '';
 
 export const ProductsApi = {
   getProducts: async (): Promise<Product[]> => {
-    const response = await apiClient.get<Product[]>(BASE_URL);
+    const response = await apiClient.get<Product[]>(`${BASE_URL}/products`);
     return response.data;
   },
 
   getProductById: async (id: number): Promise<Product> => {
-    const response = await apiClient.get<Product>(`${BASE_URL}/${id}`);
+    const response = await apiClient.get<Product>(`${BASE_URL}/products/${id}`);
     return response.data;
   }
 }; 
